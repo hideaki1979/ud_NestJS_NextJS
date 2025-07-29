f6e23d157bfa647ca95c186dcaa5bebab1ea2164
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-07-30 00:01:19 +0900

    初期作成（環境構築）(PR指摘対応)

--- nest-backend/test/app.e2e-spec.ts
+++ nest-backend/test/app.e2e-spec.ts
@@ -14,6 +14,10 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    afterEach(async () => {
+      await app.close();
+    });
   });
 
   it('/ (GET)', () => {
