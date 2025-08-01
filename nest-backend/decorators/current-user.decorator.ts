f317127dece08148a66923ceb0407ca4ac3c5d0f
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-08-01 16:37:08 +0900

    feat: Next.js環境構築（PR対応2）

--- nest-backend/decorators/current-user.decorator.ts
+++ nest-backend/decorators/current-user.decorator.ts
@@ -1,10 +1,16 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request } from 'express';
 
-export const CurrentUser = createParamDecorator((ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest<Request>();
-  if (!request.user) {
-    throw new Error('User not found in request');
-  }
-  return request.user;
-});
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    if (!request.user) {
+      throw new UnauthorizedException('User not found in request');
+    }
+    return request.user;
+  },
+);
